27fbf07a612cf85e960414ded216d176bfd69edc
EdgarEldy <ed.eldy21@live.com>
2026-07-09 21:36:00 +0400

    feat(repository): add RoleRepository
    findByRoleNameIgnoreCase/existsByRoleNameIgnoreCase mirror the
    case-insensitive email lookups on UserRepository, backing
    RoleServiceImpl's uniqueness check on create and the lookup
    UserServiceImpl.assignRole will need.

---
+++ src/main/java/edgareldy/springsecuritytutorial/repository/RoleRepository.java
@@ -0,0 +1,20 @@
+package edgareldy.springsecuritytutorial.repository;
+
+import edgareldy.springsecuritytutorial.entity.Role;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for {@link Role}.
+ * <p>
+ * Created by edgar.muhamyangabo on 7/9/26
+ * Author : edgar.muhamyangabo
+ * Date : 7/9/26
+ * Project : spring-security-tutorial
+ */
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByRoleNameIgnoreCase(String roleName);
+
+    boolean existsByRoleNameIgnoreCase(String roleName);
+}
